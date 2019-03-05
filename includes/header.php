588e123e49e3e0bd51d3b66d3b6aab225ad88784
Esther <aaa@aaa.com>
2019-03-05 13:54:24 +0100

    includes y css

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <title>Libros</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Esther,Miguel Angel,Alex">
+        <meta name="description" content="Información de libros">
+        <meta name="keywords" content="html, php, libros, información">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet">
+        <link href="../css/css1.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
